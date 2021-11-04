e576dde5693e12de90d2bb01c31696c4ce661d8e
Betty <betty_elfagid@yahoo.co.uk>
2021-11-04 10:56:08 +0000

    commit tree 6bed64cb21d0a906781acc5f8318f330faa40862

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,15 +4,29 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      message: ''
+    };
+   
+    
   }
+
+  handleChange = e => {
+    this.setState({
+      message: e.target.value
+    })
+
+    }
+
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input type="text" name="message" id="message" value={this.state.message} onChange={this.handleChange} />
+        <p>Remaining Charaters: {this.props.maxChars - this.state.message.length} </p>
       </div>
+      
     );
   }
 }
